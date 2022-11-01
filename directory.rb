fd5049a4e258112308f8fa967875f65de5ac3f54
Chris <c.bailey00@hotmail.com>
2022-11-01 21:12:26 +0000

    Enabled user to enter student names

--- directory.rb
+++ directory.rb
@@ -1,17 +1,21 @@
-# store students in array
-students = [ 
-{ name: "Dr. Hannibal Lecter", cohort: :november },
-{ name: "Darth Vader", cohort: :november },
-{ name: "Nurse Ratched", cohort: :november },
-{ name: "Michael Corleone", cohort: :november },
-{ name: "Alex DeLarge", cohort: :november },
-{ name: "The Wicked Witch of the West", cohort: :november },
-{ name: "Terminator", cohort: :november },
-{ name: "Freddy Krueger", cohort: :november },
-{ name: "The Joker", cohort: :november },
-{ name: "Joffrey Baratheon", cohort: :november },
-{ name: "Norman Bates", cohort: :november }
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # loop while name isn't empty
+  while !name.empty? do
+    # push student hash to student array
+    students << { name: name, cohort: :november}
+    puts "now we have #{students.count} students"
+    # get next name
+    name = gets.chomp
+  end
+  #return students
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -26,6 +30,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
