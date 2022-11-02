13385107e791b9e3cf60e4d7757ab791fd5f1636
Chris <c.bailey00@hotmail.com>
2022-11-02 17:31:41 +0000

    added ability to input cohort month

--- directory_step_8_copy.rb
+++ directory_step_8_copy.rb
@@ -1,4 +1,18 @@
 def input_students
+  months = [
+    :january,
+    :february,
+    :march,
+    :april,
+    :may,
+    :june,
+    :july,
+    :august,
+    :september,
+    :october,
+    :november,
+    :december
+  ]
   puts "Please enter the names of the students"
   puts "To finish, just hit return with the name field blank"
   # create an empty array
@@ -7,12 +21,17 @@ def input_students
   name = gets.chomp
   # loop while name isn't empty
   while !name.empty? do
+    while true do
+      puts "Which cohort?"; cohort = gets.chomp.downcase.to_sym
+      if cohort.empty? || months.include?(cohort) then break end
+    end
+    cohort = :november if cohort.empty?
     puts "Enter a hobby, country of birth, height. Or leave blank"
     puts "Hobby: "; hobby = gets.chomp
     puts "Country of birth: "; birth = gets.chomp
     puts "Height: "; height = gets.chomp
     # push student hash to student array
-    students << { name: name, cohort: :november, hobby: hobby, birth: birth, height: height }
+    students << { name: name, cohort: cohort, hobby: hobby, birth: birth, height: height }
     puts "now we have #{students.count} students"
     # get next name
     puts "Enter name:"
@@ -23,20 +42,20 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(50)
+  puts "-------------".center(50)
 end
 
 def print(names)
   i = 0
   while i < names.length
-    puts "#{i + 1}. #{names[i][:name]} cohort: #{names[i][:cohort]}"
+    puts "#{i + 1}. #{names[i][:name]} cohort: #{names[i][:cohort]}".center(50)
     i += 1
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students".center(50)
 end
 
 students = input_students
