506141e596f0068d1d03ec5dd7b3374c2f3dfa03
Chris <c.bailey00@hotmail.com>
2022-11-03 15:33:05 +0000

    Now asks for filename to save or load, to or from

--- directory.rb
+++ directory.rb
@@ -1,25 +1,15 @@
 @students = []
 
 def input_students
-  months = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december]
   puts "Please enter the names of the students"
   puts "To finish, just hit return with the name field blank"
-
   # get the first name
   name = STDIN.gets.strip
-
   # loop while name isn't empty
   while !name.empty? do
-    while true do
-      puts "Which cohort?"; cohort = STDIN.gets.strip.downcase.to_sym
-      if cohort.empty? || months.include?(cohort) then break end
-    end
-    cohort = :november if cohort.empty?
-
     # push student hash to student array
-    add_student(name,cohort)
-    @students.length == 1 ? (puts "now we have #{@students.count} student") : (puts "now we have #{@students.count} students")
-
+    add_student(name,:november)
+    puts "now we have #{@students.count} students"
     # get next name
     puts "Enter name:"
     name = STDIN.gets.chomp
@@ -36,8 +26,8 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
+  puts "3. Save the list"
+  puts "4. Load the list"
   puts "9. Exit"
 end
 
@@ -72,32 +62,15 @@ def print_header
 end
 
 def print_students_list
-  names_sorted = {}
-  @students.map { |name| 
-    if names_sorted[name[:cohort]].nil? then names_sorted[name[:cohort]] = [] end
-    names_sorted[name[:cohort]] << name[:name]
-    }
-  names_sorted.each{ |cohort,names| 
-    i = 0
-    puts "#{cohort}:\n".center(50)
-    while i < names.length
-      puts names[i].center(50)
-      i += 1
-    end
-    puts "---".center(50)
-  }
+  @students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
 end
 
 def print_footer
-  if @students.length == 1
-    puts "Overall, we have #{@students.count} great student".center(50)
-  else
     puts "Overall, we have #{@students.count} great students".center(50)
-  end
 end
 
 def add_student(name,cohort)
-  @students << {name: name, cohort: cohort.to_sym}
+  @students << {name: name, cohort: cohort}
 end
 
 def try_load_students
@@ -110,6 +83,10 @@ def try_load_students
 end
 
 def load_students(filename = "students.csv")
+  puts "Enter filename to load from. Leave blank for default"
+  filename = STDIN.gets.chomp
+  if filename.empty? then filename = "students.csv" end
+  puts "loading students from #{filename}"
   file = File.open(filename,"r")
   file.readlines.each { |line| 
     name, cohort = line.chomp.split(",")
@@ -119,6 +96,10 @@ def load_students(filename = "students.csv")
 end
 
 def save_students
+  puts "Enter filename to save to. Leave blank for default"
+  filename = STDIN.gets.chomp
+  if filename.empty? then filename = "students.csv" end
+  puts "saving students to #{filename}"
   # open the file for writing
   file = File.open("students.csv", "w")
   # iterate over the array of students
--- directory_copy.rb
+++ directory_copy.rb
@@ -1,18 +1,26 @@
 def input_students
+  months = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december]
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
+
   # create an empty array
   students = []
   # get the first name
   name = gets.chomp
   # loop while name isn't empty
   while !name.empty? do
+    while true do
+      puts "Which cohort?"; cohort = gets.strip.downcase.to_sym
+      if cohort.empty? || months.include?(cohort) then break end
+    end
+    cohort = :november if cohort.empty?
+
     puts "Enter a hobby, country of birth, height. Or leave blank"
     puts "Hobby: "; hobby = gets.strip
     puts "Country of birth: "; birth = gets.strip
     puts "Height: "; height = gets.strip
     # push student hash to student array
-    students << { name: name, cohort: :november, hobby: hobby, birth: birth, height: height}
+    students << { name: name, cohort: cohort, hobby: hobby, birth: birth, height: height}
     puts "now we have #{students.count} students"
     # get next name
     name = gets.chomp
@@ -27,14 +35,29 @@ def print_header
 end
 
 def print(names)
-  puts "Would you like to search for students whose name: begins with a specific letter (1), is less than 12 characters (2), or print all names (3)?"
+  puts "Would you like to search for students whose name: begins with a specific letter (1), is less than 12 characters (2), is sorted by cohort (3), or print all names (4)?"
   answer = gets.chomp.to_i
   if answer == 1
     puts "Enter a letter"
     letter = gets.chomp
     names.each_with_index { |name,index| if name[:name][0] == letter then puts "#{index + 1}. #{name[:name]} (#{name[:cohort]} cohort)" end }
-  elsif answer = 2
+  elsif answer == 2
     names.each_with_index { |name,index| if name[:name].length  < 12 then puts "#{index + 1}. #{name[:name]} (#{name[:cohort]} cohort)" end }
+  elsif answer == 3
+    names_sorted = {}
+    @students.map { |name| 
+      if names_sorted[name[:cohort]].nil? then names_sorted[name[:cohort]] = [] end
+      names_sorted[name[:cohort]] << name[:name]
+    }
+    names_sorted.each{ |cohort,names| 
+      i = 0
+      puts "#{cohort}:\n".center(50)
+      while i < names.length
+        puts names[i].center(50)
+        i += 1
+      end
+      puts "---".center(50)
+    }
   else
     names.each_with_index { |name,index| puts "#{index + 1}. #{name[:name]} (#{name[:cohort]} cohort)" }
   end
